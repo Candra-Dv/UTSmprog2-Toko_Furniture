227f1ef6f67ca13706b7029fbf8d66fd65022ce7
Candra-dev <84716211+Candra-dev@users.noreply.github.com>
2021-12-31 10:17:11 +0700

    penembahan detail barang

---
+++ app/src/main/java/com/example/utsmprog2_tokofurniture/DetailBarang.java
@@ -0,0 +1,14 @@
+package com.example.utsmprog2_tokofurniture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailBarang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_barang);
+    }
+}
---
+++ app/src/main/java/com/example/utsmprog2_tokofurniture/HalBeli.java
@@ -0,0 +1,14 @@
+package com.example.utsmprog2_tokofurniture;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HalBeli extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hal_beli);
+    }
+}
